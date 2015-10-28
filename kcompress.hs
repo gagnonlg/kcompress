8e340a0d313982aa88002ff3f6949dd4fac51af1
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2015-10-28 16:20:38 -0400

    Add copyright permission statement to source file

--- kcompress.hs
+++ kcompress.hs
@@ -1,5 +1,22 @@
 {-# LANGUAGE FlexibleInstances #-}
 
+{-
+Copyright 2015 Louis-Guillaume Gagnon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 import Codec.Picture
 import Codec.Picture.Types
 import Data.Set
@@ -120,7 +137,6 @@ pixelNorm (PixelRGB16 r g b) =
 
 initialize :: [PixelRGB16] -> Set (Assignement PixelRGB16)
 initialize pxs = fromList $ Prelude.map (\p -> Assignement p (PixelRGB16 0 0 0) 0 empty) pxs
-
 
 toImage :: Set (Assignement PixelRGB16) -> Image PixelRGB16 -> Image PixelRGB16
 toImage as img = generateImage indiceMap (imageWidth img) (imageHeight img)
